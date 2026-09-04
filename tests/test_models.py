8238339fa389b26c434011174c39494c8cb48541
Naveen Anandhan <antiquenaveen@gmail.com>
2026-09-04 16:12:39 +0200

    chore: remove Python tests

--- tests/test_models.py
+++
@@ -1,9 +0,0 @@
-from app.schemas import ImportRequest, SyncRequest
-
-def test_import_request():
-    x=ImportRequest(scope_type='feature', scope_value='F123', dry_run=True)
-    assert x.include_tasks is True
-
-def test_sync_request():
-    x=SyncRequest(scope_type='all')
-    assert x.direction == 'reconcile'
